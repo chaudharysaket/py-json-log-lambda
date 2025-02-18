646396e56e95ca8daf3af07848a5d46d3bf6b49a
Saket Chaudhary <118331906+chaudharysaket@users.noreply.github.com>
2025-02-18 14:45:52 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,13 +3,10 @@ import json
 import uuid
 from datetime import datetime
 
-# Define the JSON formatter class
 class JSONFormatter(logging.Formatter):
     def format(self, record):
-
         log_message = {
-            'level': record.levelname.lower(),
-            'request_id': getattr(record, 'request_id', None) or str(uuid.uuid4()),  # Use request_id from log record if available
+            'injected_request_id': getattr(record, 'request_id', None) 
             'message': record.getMessage()
         }
         return json.dumps(log_message)
@@ -22,12 +19,12 @@ console_handler.setFormatter(JSONFormatter())
 logger.addHandler(console_handler)
 
 def lambda_handler(event, context):
-    # Create a logger adapter to add custom attributes (requestId in this case)
-    extra = {'request_id': context.aws_request_id}
-    custom_logger = logging.LoggerAdapter(logger, extra)
 
-    # Log a message with the custom attributes
-    custom_logger.info("Lambda function started")
+    injected_request_id = {'request_id': context.aws_request_id}
+    custom_logger = logging.LoggerAdapter(logger, injected_request_id)
+
+
+    custom_logger.info("Test nLambda Log")
 
     # Simulate some processing
     try:
